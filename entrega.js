f052b4fb23a0853f1fcf58413df9c40336080a55
asu88 <asu.ramon88@gmail.com>
2015-02-07 15:58:18 +0100

    Version simple con changer

--- entrega.js
+++ entrega.js
@@ -1,27 +1,21 @@
 
-  var ran = function photosRandom(id){
+  function changer(){
     
     
-    var element = document.getElementById(id);
-    var urls = ["http://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Metro_SPB_Line3_Obukhovo.jpg/1024px-Metro_SPB_Line3_Obukhovo.jpg","http://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Postsparkasse_Otto_Wagner_Beamtenstiege_2.JPG/640px-Postsparkasse_Otto_Wagner_Beamtenstiege_2.JPG","http://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Catedral%2C_Gniezno%2C_Polonia%2C_2012-12-24%2C_DD_01.jpg/1280px-Catedral%2C_Gniezno%2C_Polonia%2C_2012-12-24%2C_DD_01.jpg"];
+    var element = document.getElementById("foto");
     
-    
-   // var string = element.innerHTML;
-      // console.log("array " + string.split("+"));
-    //var operador = string.split("+");
-    //var resultado = parseInt(operador[0]) + parseInt (operador[1]);
-     //  console.log(resultado);
-    // element.innerHTML = resultado;
+
+    var fotos = Array("http://bit.ly/1unaRGE","http://bit.ly/1KrMY4H","http://bit.ly/1A0LaN1");
+
+    var foto = fotos[Math.floor(Math.random()*fotos.length)];
      
-    element.innerHTML = '<img src ='+urls[1]+'></img>';
+    element.innerHTML = '<img src ='+foto+'></img>';
     
     
-    //elem2.innerHTML =  " = "+resultado;
+    
 
 
   }
   
-   		 ran("foto");
+   		 changer();
 
-  	
-  //gresy Forg
