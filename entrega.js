a4a4791adb8fbf378b7a4ca88fe9a7240705d2a4
asu88 <asu.ramon88@gmail.com>
2015-02-08 00:03:46 +0100

    Primera forma

--- entrega.js
+++ entrega.js
@@ -1,21 +1,21 @@
 
-  function changer(){
+  function setFoto(id, value){
     
-    
-    var element = document.getElementById("foto");
-    
+   var element = document.getElementById(id); 
+   element.innerHTML = value;
 
-    var fotos = Array("http://bit.ly/1unaRGE","http://bit.ly/1KrMY4H","http://bit.ly/1A0LaN1");
-
-    var foto = fotos[Math.floor(Math.random()*fotos.length)];
-     
-    element.innerHTML = '<img src ='+foto+'></img>';
     
     
-    
-
-
-  }
+    }
   
-   		 changer();
+   
+    function photosRandom(){
 
+         var fotos = ["http://bit.ly/1unaRGE","http://bit.ly/1KrMY4H","http://bit.ly/1A0LaN1",
+         "http://bit.ly/1A2IyhA","http://bit.ly/1A2IVsw","http://bit.ly/1yUEUlF",
+         "http://bit.ly/1AGWjE3","http://bit.ly/1vxYfNB","http://bit.ly/1zu4ktX"];
+         var foto = fotos[Math.floor(Math.random()*fotos.length)];
+        
+        var NewValue = '<img src ='+foto+' with=80% height=70%></img>';
+        setFoto("foto", NewValue);
+}
